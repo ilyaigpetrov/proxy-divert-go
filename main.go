fc9fe8aa345ab947fdba2568cb65f02259c5be8d
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-12-18 17:57:17 +0500

    Divert and client work together

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
   "fmt"
   "log"
   "time"
+  "bytes"
   "os"
+  "io"
+  //"encoding/hex"
   "net"
   "os/signal"
   "github.com/clmul/go-windivert"
-  "golang.org/x/sys/windows"
   "github.com/google/gopacket"
   "github.com/google/gopacket/layers"
 )
@@ -21,6 +23,14 @@ var DIVERT_NO_LOCALNETS_DST = `(
   (ip.DstAddr < 172.16.0.0 or ip.DstAddr > 172.31.255.255) and
   (ip.DstAddr < 169.254.0.0 or ip.DstAddr > 169.254.255.255)
 )`
+
+var Error = log.New(os.Stderr,
+    "ERROR: ",
+    log.Ldate|log.Ltime|log.Lshortfile)
+
+var Info = log.New(os.Stdout,
+    "INFO: ",
+    log.Ldate|log.Ltime|log.Lshortfile)
 
 var remote net.Conn
 var isDisconnected = make(chan struct{})
@@ -42,7 +52,7 @@ func connectTo(serverPoint string) (ifConnected bool) {
 func keepConnectedTo(serverPoint string) {
 
   if connectTo(serverPoint) == false {
-    //Error.Fatal("Failed to stick to this server.")
+    Error.Fatal("Failed to stick to this server.")
   }
   for _ = range isDisconnected {
     if remote != nil {
@@ -50,24 +60,17 @@ func keepConnectedTo(serverPoint string) {
     }
     for {
       ok := connectTo(serverPoint)
-      if !ok {
-        fmt.Println("Reconnect in 5 seconds")
-        time.Sleep(time.Second * 5)
+      if ok {
+        break
       }
+      fmt.Println("Reconnect in 5 seconds")
+      time.Sleep(time.Second * 5)
     }
   }
 
 }
 
 func main() {
-
-  Error := log.New(os.Stderr,
-    "ERROR: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
-
-  Info := log.New(os.Stdout,
-    "INFO: ",
-    log.Ldate|log.Ltime|log.Lshortfile)
 
   if len(os.Args) != 2 {
     fmt.Printf("Usage: %s proxy_address:port\n", filepath.Base(os.Args[0]))
@@ -107,13 +110,14 @@ func main() {
   }()
 
   maxPacketSize := 9016
-  packetData := make([]byte, maxPacketSize)
+  packetBuffer := make([]byte, maxPacketSize)
   for {
-    _, _, err := handle.Recv(packetData)
+    n, _, err := handle.Recv(packetBuffer)
     if err != nil {
       Error.Println(err)
       continue
     }
+    packetData := packetBuffer[:n]
 
     packet := gopacket.NewPacket(packetData, layers.LayerTypeIPv4, gopacket.Default)
     ipLayer := packet.Layer(layers.LayerTypeIPv4)
@@ -121,11 +125,6 @@ func main() {
         fmt.Println("IPv4 layer detected.")
         ip, _ := ipLayer.(*layers.IPv4)
 
-        // IP layer variables:
-        // Version (Either 4 or 6)
-        // IHL (IP Header Length in 32-bit words)
-        // TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-        // Checksum, SrcIP, DstIP
         fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
         fmt.Println("Protocol: ", ip.Protocol)
     } else {
@@ -133,22 +132,11 @@ func main() {
       continue
     }
 
-    s, err := windows.Socket(windows.AF_INET, windows.SOCK_RAW, windows.IPPROTO_TCP)
+    _, err = io.Copy(remote, bytes.NewReader(packetData))
     if err != nil {
       Error.Println(err)
-      continue
+      isDisconnected <- struct{}{}
     }
-
-    var arr [4]byte
-    copy(arr[:], tcpAddr.IP.To4()[:4])
-    addr := windows.SockaddrInet4{
-      Addr: arr,
-    }
-
-    //addr := ip2int(header.Dst)
-
-    //sin.sin_addr = C.struct_in_addr{ s_addr: addr }
-    windows.Sendto (s, packetData, 0, &addr)
 
   }
 
--- proxy-client-win.go
+++ proxy-client-win.go
@@ -1,108 +1,236 @@
 package main
 
 import (
-	"io"
-	"net"
-	"sync"
+  "io"
+  "net"
+  "sync"
   "fmt"
+  "flag"
+  "golang.org/x/net/ipv4"
+  //"encoding/hex"
+  "os"
+  "os/signal"
 
-	log "github.com/Sirupsen/logrus"
+  log "github.com/Sirupsen/logrus"
+  "github.com/google/gopacket"
+  "github.com/google/gopacket/layers"
 )
 
+const SO_ORIGINAL_DST = 80
+
 type Proxy struct {
-	from string
-	to   string
-	done chan struct{}
-	log  *log.Entry
+  from string
+  fromTCP *net.TCPAddr
+  done chan struct{}
+  log  *log.Entry
 }
 
-func NewProxy(from, to string) *Proxy {
-	return &Proxy{
-		from: from,
-		to:   to,
-		done: make(chan struct{}),
-		log: log.WithFields(log.Fields{
-			"from": from,
-			"to":   to,
-		}),
-	}
+func NewProxy(from string) *Proxy {
+
+  log.SetLevel(log.InfoLevel)
+  return &Proxy{
+    from: from,
+    done: make(chan struct{}),
+    log: log.WithFields(log.Fields{
+      "from": from,
+    }),
+  }
+
 }
 
 func (p *Proxy) Start() error {
-	p.log.Infoln("Starting proxy")
-	listener, err := net.Listen("tcp", p.from)
-	if err != nil {
-		return err
-	}
-	go p.run(listener)
-	return nil
+  p.log.Infoln("Starting proxy")
+  var err error
+  p.fromTCP, err = net.ResolveTCPAddr("tcp", p.from)
+  if (err != nil) {
+    panic(err)
+  }
+  listener, err := net.ListenTCP("tcp", p.fromTCP)
+  if err != nil {
+    return err
+  }
+  go p.run(*listener)
+  return nil
 }
 
 func (p *Proxy) Stop() {
-	p.log.Infoln("Stopping proxy")
-	if p.done == nil {
-		return
-	}
-	close(p.done)
-	p.done = nil
+  p.log.Infoln("Stopping proxy")
+  if p.done == nil {
+    return
+  }
+  close(p.done)
+  p.done = nil
 }
 
-func (p *Proxy) run(listener net.Listener) {
-	for {
-		select {
-		case <-p.done:
-			return
-		default:
-			connection, err := listener.Accept()
-			if err == nil {
-				go p.handle(connection)
-			} else {
-				p.log.WithField("err", err).Errorln("Error accepting conn")
-			}
-		}
-	}
+
+func (p *Proxy) run(listener net.TCPListener) {
+  for {
+    select {
+    case <-p.done:
+      return
+    default:
+      connection, err := listener.AcceptTCP()
+      if connection == nil {
+        p.log.WithField("err", err).Errorln("Nil connection")
+        panic(err)
+      }
+      la := connection.LocalAddr()
+      if (la == nil) {
+        panic("Connection lost!")
+      }
+      fmt.Printf("Connection from %s\n", la.String())
+
+      if err == nil {
+        go p.handle(*connection)
+      } else {
+        p.log.WithField("err", err).Errorln("Error accepting conn")
+      }
+    }
+  }
 }
 
-func (p *Proxy) handle(connection net.Conn) {
-	p.log.Debugln("Handling", connection)
-	defer p.log.Debugln("Done handling", connection)
-	defer connection.Close()
-  fmt.Printf("Connection to %s\n", connection.RemoteAddr().String())
+func (p *Proxy) handle(connection net.TCPConn) {
 
-  return
+  defer connection.Close()
+  p.log.Debugln("Handling", connection)
+  defer p.log.Debugln("Done handling", connection)
 
-	remote, err := net.Dial("tcp", p.to)
-	if err != nil {
-		p.log.WithField("err", err).Errorln("Error dialing remote host")
-		return
-	}
-	defer remote.Close()
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-	go p.copy(remote, connection, wg)
-	go p.copy(connection, remote, wg)
-	wg.Wait()
+  buf := make([]byte, 0, 8186) // big buffer
+  tmp := make([]byte, 4096)     // using small tmo buffer for demonstrating
+  for {
+    n, err := connection.Read(tmp)
+    if err != nil {
+      if err != io.EOF {
+            fmt.Println("read error:", err)
+        }
+        break
+    }
+    fmt.Println("got", n, "bytes.")
+    buf = append(buf, tmp[:n]...)
+    header, err := ipv4.ParseHeader(buf)
+    if err != nil {
+      fmt.Println("Couldn't parse packet, dropping connnection.")
+      return
+    }
+    if header.TotalLen == 0 && len(buf) > 0 {
+      fmt.Println("Buffer is not parserable!")
+      os.Exit(1)
+    }
+    if (header.TotalLen > len(buf)) {
+      fmt.Println("Reading more up to %d\n", header.TotalLen)
+      continue
+    }
+    packetData := buf[0:header.TotalLen]
+    fmt.Printf("PACKET LEN:%d, bufLen:%d\n", header.TotalLen, len(buf))
+
+    buf = buf[header.TotalLen:]
+
+    fmt.Printf("Packet to %s\n", header.Dst)
+    //fmt.Println(hex.Dump(packetData))
+
+    var src, dst string
+
+    packet := gopacket.NewPacket(packetData, layers.LayerTypeIPv4, gopacket.Default)
+    ipLayer := packet.Layer(layers.LayerTypeIPv4)
+    if ipLayer != nil {
+        fmt.Println("IPv4 layer detected.")
+        ip, _ := ipLayer.(*layers.IPv4)
+
+        src = ip.SrcIP.String()
+        dst = ip.DstIP.String()
+    } else {
+      fmt.Println("No IP layer!")
+      continue
+    }
+
+    if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+        tcp, _ := tcpLayer.(*layers.TCP)
+        dst = fmt.Sprintf("%s:%d", dst, tcp.DstPort)
+        src = fmt.Sprintf("%s:%d", src, tcp.SrcPort)
+    } else {
+      fmt.Println("NOT TCP!")
+      continue
+    }
+    fmt.Printf("From %s to %s\n", src, dst)
+
+  }
+
+
+  /*
+
+  dest := ipv4 + ":" + fmt.Sprintf("%d", port)
+  if dest == *remoteAddr || dest == strings.Replace(*remoteAddr, "0.0.0.0", "127.0.0.1", -1) {
+    fmt.Printf("DESTINATION IS SELF: %s", dest)
+    return // NO SELF CONNECTIONS
+  }
+
+  addr, err := net.ResolveTCPAddr("tcp", dest)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("Connection to %s\n", dest)
+  remote, err := net.DialTCP("tcp", nil, addr)
+  if err != nil {
+    p.log.WithField("err", err).Errorln("Error dialing remote host")
+    return
+  }
+  defer remote.Close()
+  wg := &sync.WaitGroup{}
+  wg.Add(2)
+  go p.copy(*remote, connection, wg)
+  go p.copy(connection, *remote, wg)
+  wg.Wait()
+  */
+
 }
 
-func (p *Proxy) copy(from, to net.Conn, wg *sync.WaitGroup) {
-	defer wg.Done()
-	select {
-	case <-p.done:
-		return
-	default:
-		if _, err := io.Copy(to, from); err != nil {
-			p.log.WithField("err", err).Errorln("Error from copy")
-			p.Stop()
-			return
-		}
-	}
+func (p *Proxy) copy(from, to net.TCPConn, wg *sync.WaitGroup) {
+  defer wg.Done()
+  select {
+  case <-p.done:
+    return
+  default:
+    if _, err := io.Copy(&to, &from); err != nil {
+      p.log.WithField("err", err).Errorln("Error from copy")
+      p.Stop()
+      return
+    }
+  }
 }
+
+func itod(i uint) string {
+        if i == 0 {
+                return "0"
+        }
+
+        // Assemble decimal in reverse order.
+        var b [32]byte
+        bp := len(b)
+        for ; i > 0; i /= 10 {
+                bp--
+                b[bp] = byte(i%10) + '0'
+        }
+
+        return string(b[bp:])
+}
+
+var remoteAddr *string = flag.String("r", "boom", "remote address")
 
 func main() {
 
-  p := NewProxy("0.0.0.0:2222", "127.0.0.1:1111")
-  p.Start()
-  fmt.Println("Listening on 0.0.0.0:2222")
-  select{}
+    controlC := make(chan os.Signal)
+    signal.Notify(controlC, os.Interrupt)
+    go func(){
+      <-controlC
+      fmt.Println("Exiting after Ctrl+C")
+      os.Exit(0)
+    }()
 
+
+    flag.Parse();
+    log.SetLevel(log.InfoLevel)
+
+    NewProxy(*remoteAddr).Start()
+    fmt.Println("Server started.")
+    select{}
 }
